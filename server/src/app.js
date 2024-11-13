6267a93a301acc6511ff8eab2a9ec36fcae158a0
Rachit Jain <rachitjainjaipur@gmail.com>
2024-11-13 22:55:14 +0530

    login logout verify jwt done

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,13 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+import userRouter from "./routes/user.route.js"
+
+app.use("/api/v1", userRouter)
 
 export default app;
